6f4119768c9a91fe5e9104d94bed833987cb5a08
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-04-24 17:35:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 from ycmd.ycmd import CMD
 from ycmd.ClassData import ClassData
+import re
+import urllib.parse
+import urllib.request
+import asyncio
+import discord
+import os
+import json
+import datetime
+import random
+import time
+import viewbotgui
+from hashids import Hashids
+# load our local env so we dont have the token in public
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from discord import TextChannel
+from youtube_dl import YoutubeDL
 
-cmd = CMD()
-await cmd.cmdrun_file("main.ycmd")
+
+client = commands.Bot(command_prefix='!')
+@client.event  # check if bot is ready
+async def on_ready():
+    cmd = CMD()
+    cmd.var["bot"]=ClassData(client)
+    await cmd.cmdrun_method_file("main.ycmd","on_ready")
+@client.event
+async def on_message(message):
+    cmd = CMD()
+    cmd.var["message"]=ClassData(messgae)
+    cmd.var["bot"]=ClassData(client)
+    await cmd.cmdrun_method_file("main.ycmd","on_message")
+client.run('token')
