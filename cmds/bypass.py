34693604a00ff7868908790e3fd066706df163ec
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-06 18:56:56 +0900

    Create bypass.py

---
+++ cmds/bypass.py
@@ -0,0 +1,7 @@
+import ycmd
+def nextqueue(gid,ocmd):
+  ncmd = ycmd.ycmd.CMD()
+  ncmd.var['ocmd'] = ocmd
+  ncmd.var['gid'] = gid
+  loop = asyncio.get_event_loop()
+  asyncio.run_coroutine_threadsafe(ncmd.cmdrun_method_file("cmds/play.ycmd","nextqueue"),loop)
