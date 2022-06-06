c1d0e50604735f4c46dd73e52a14506fb5b884eb
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-06 22:40:52 +0900

    Update bypass.py

--- botcmd/bypass.py
+++ botcmd/bypass.py
@@ -1,7 +1,8 @@
 import ycmd
-def nextqueue(gid,ocmd):
-  ncmd = ycmd.ycmd.CMD()
-  ncmd.var['ocmd'] = ocmd
-  ncmd.var['gid'] = gid
-  loop = asyncio.get_event_loop()
-  asyncio.run_coroutine_threadsafe(ncmd.cmdrun_method_file("botcmd/play.ycmd","nextqueue"),loop)
+class bypass(): 
+  def nextqueue(gid,ocmd):
+    ncmd = ycmd.ycmd.CMD()
+    ncmd.var['ocmd'] = ocmd
+    ncmd.var['gid'] = gid
+    loop = asyncio.get_event_loop()
+    asyncio.run_coroutine_threadsafe(ncmd.cmdrun_method_file("botcmd/play.ycmd","nextqueue"),loop)
