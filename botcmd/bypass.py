300657a133cb0dc8f168ee6e12cee623c962bd8c
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-06-06 23:44:18 +0900

    Add files via upload

--- botcmd/bypass.py
+++ botcmd/bypass.py
@@ -2,10 +2,10 @@ import ycmd
 import asyncio
 class bypass(): 
   def ply(self,gid,ocmd):
+    loop = asyncio.get_event_loop()
     def nextqueue(e):
       ncmd = ycmd.ycmd.CMD()
       ncmd.var['ocmd'] = ocmd
       ncmd.var['gid'] = gid
-      loop = asyncio.new_event_loop()
       asyncio.run_coroutine_threadsafe(ncmd.cmdrun_method_file("botcmd/play.ycmd","nextqueue"),loop)
     ocmd.getvar('vcc').play(ocmd.getvar('fpa'),after=nextqueue)
